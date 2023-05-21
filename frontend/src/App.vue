8492cb4d00b1329fe3ac9c3480dcc94d9a030198
Alain Tran <alaint77@gmail.com>
2023-05-21 16:43:33 +0200

    clean useless files and add main content

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,67 @@
-<script setup>
-
-</script>
-
 <template>
-  <h1 class="text-3xl font-bold underline">Developer Frontend</h1>
-  <div class="title">Alain Tran</div>
-  <p>new site soon.......</p>
+  <div class="grid place-content-center max-w-4xl min-h-screen  w-full mx-auto p-6">
+    <h1 class="mb-6 text-3xl font-bold text-center">Alain Tran <br> Frontend Developer</h1>
+    <h2 class="text-base font-bold text-center text-neutral-600 italic">Preparing new design for my website! <br> (Last update 21/05/2023)</h2>
+  
+    <div class="mx-auto my-6">
+      <h2 class="my-6 text-2xl font-bold">Currently working for <a href="https://www.opsone.net/" target="_blank" class="inline-block p-2 text-[#ffd32f] bg-neutral-900 rotate-2 leading-7">Opsone</a> as Frontend Developer (since November 2019)</h2>
+      <ul class="grid xs:grid-cols-2 gap-6">
+        <li class="first-letter:mr-3 first-letter:text-4xl first-letter:font-bold first-letter:float-left">
+          Journeying into the world of frontend development specializing in 
+          building awesome user interfaces for websites and apps. I bring 
+          designs to life and make sure everything runs smoothly. I'm all about 
+          making things look great, work well, and provide a user-friendly 
+          experience.
+        </li>
+        <li class="first-letter:mr-3 first-letter:text-4xl first-letter:font-bold first-letter:float-left">
+          Simply put, I find joy in writing clean and organized code! I strive 
+          for efficiency, following best practices and coding standards. This way, 
+          the codebase is easy to understand, collaborate on, and ready for 
+          future growth.
+        </li>
+      </ul>
+    </div>
+  
+    <div class="grid xs:grid-cols-2 gap-6">
+      <div>
+        <p class="mb-4 text-2xl font-bold">Skills</p>
+        <ul>
+          <li>JavaScript</li>
+          <li>CSS / SCSS / TailwindCSS</li>
+          <li>Responsive UI</li>
+        </ul>
+      </div>
+
+      <div>
+        <p class="mb-4 text-2xl font-bold">Notable projects with my amazing team</p>
+        <ul class="flex gap-4 flex-wrap">
+          <li>
+            <a href="https://www.lustucru-selection.fr" target="_blank">Lustucru Selection</a>
+          </li>
+          <li>
+            <a href="https://www.panzanifoodservice.com" target="_blank">Panzani Food Service</a>
+          </li>
+          <li>
+            <a href="https://www.opsone.net" target="_blank">Opsone</a>
+          </li>
+          <li>
+            <a href="https://www.belairmonange.com" target="_blank">Bélair-Monange</a>
+          </li>
+          <li>
+            <a href="https://www.delphiayachts.com" target="_blank">Delphiayachts</a>
+          </li>
+          <li>
+            <a href="https://www.beneteau.com" target="_blank">Beneteau</a>
+          </li>
+          <li>
+            <a href="https://www.timevallee.com" target="_blank">TimeVallée</a>
+          </li>
+          <li>
+            <a href="https://www.enacr.com/" target="_blank">Enacr (Homepage)</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-  .title {
-    color: palevioletred;
-  }
-</style>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'xs': '480px',
+    },
   },
   plugins: [],
 }
