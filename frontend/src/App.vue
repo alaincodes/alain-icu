8e8bd9c63fc3dd2b027d701b9de69d6a7ae7df4f
Alain Tran <alaint77@gmail.com>
2023-05-21 20:36:30 +0200

    add socials link

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,31 @@
 <template>
-  <div class="grid place-content-center max-w-4xl min-h-screen  w-full mx-auto p-6">
-    <h1 class="mb-6 text-3xl font-bold text-center">Alain Tran <br> Frontend Developer</h1>
+  <div class="grid place-content-center gap-7 max-w-4xl min-h-screen  w-full mx-auto p-6">
+    <h1 class="text-3xl font-bold text-center">Alain Tran <br> Frontend Developer</h1>
     <h2 class="text-base font-bold text-center text-neutral-600 italic">Preparing new design for my website! <br> (Last update 21/05/2023)</h2>
   
-    <div class="mx-auto my-6">
-      <h2 class="my-6 text-2xl font-bold">Currently working for <a href="https://www.opsone.net/" target="_blank" class="inline-block p-2 text-[#ffd32f] bg-neutral-900 rotate-2 leading-7">Opsone</a> as Frontend Developer (since November 2019)</h2>
+    <ul class="grid grid-flow-col justify-center gap-6">
+      <li>
+        <a class="inline-block w-8 h-8 max-w-full" href="https://twitter.com/alaintran90">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-48.9 158.8c.2 2.8.2 5.7.2 8.5 0 86.7-66 186.6-186.6 186.6-37.2 0-71.7-10.8-100.7-29.4 5.3.6 10.4.8 15.8.8 30.7 0 58.9-10.4 81.4-28-28.8-.6-53-19.5-61.3-45.5 10.1 1.5 19.2 1.5 29.6-1.2-30-6.1-52.5-32.5-52.5-64.4v-.8c8.7 4.9 18.9 7.9 29.6 8.3a65.447 65.447 0 0 1-29.2-54.6c0-12.2 3.2-23.4 8.9-33.1 32.3 39.8 80.8 65.8 135.2 68.6-9.3-44.5 24-80.6 64-80.6 18.9 0 35.9 7.9 47.9 20.7 14.8-2.8 29-8.3 41.6-15.8-4.9 15.2-15.2 28-28.8 36.1 13.2-1.4 26-5.1 37.8-10.2-8.9 13.1-20.1 24.7-32.9 34z"/></svg>
+          <span class="sr-only">my Twitter</span>
+        </a>
+      </li>
+      <li>
+        <a class="inline-block w-8 h-8 max-w-full" href="https://www.linkedin.com/in/alain-tran-6a95071ba/" target="_blank">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>
+          <span class="sr-only">my LinkedIn</span>
+        </a>
+      </li>
+      <li>
+        <a class="inline-block w-8 h-8 max-w-full" href="mailto:alaint77@gmail.com" target="_blank">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M256 64C150 64 64 150 64 256s86 192 192 192c17.7 0 32 14.3 32 32s-14.3 32-32 32C114.6 512 0 397.4 0 256S114.6 0 256 0S512 114.6 512 256v32c0 53-43 96-96 96c-29.3 0-55.6-13.2-73.2-33.9C320 371.1 289.5 384 256 384c-70.7 0-128-57.3-128-128s57.3-128 128-128c27.9 0 53.7 8.9 74.7 24.1c5.7-5 13.1-8.1 21.3-8.1c17.7 0 32 14.3 32 32v80 32c0 17.7 14.3 32 32 32s32-14.3 32-32V256c0-106-86-192-192-192zm64 192a64 64 0 1 0 -128 0 64 64 0 1 0 128 0z"/></svg>
+          <span class="sr-only">my E-mail</span>
+        </a>
+      </li>
+    </ul>
+
+    <div class="mx-auto">
+      <h2 class="mb-7 text-2xl font-bold">Currently working for <a href="https://www.opsone.net/" target="_blank" class="inline-block p-2 text-[#ffd32f] bg-neutral-900 rotate-2 leading-7">Opsone</a> as Frontend Developer (since November 2019)</h2>
       <ul class="grid xs:grid-cols-2 gap-6">
         <li class="first-letter:mr-3 first-letter:text-4xl first-letter:font-bold first-letter:float-left">
           Journeying into the world of frontend development specializing in 
