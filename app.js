55522a398fb8a131b3c916110e73e9a4fb123cca
Alain Tran <alaint77@gmail.com>
2023-05-21 12:41:51 +0200

    init commit for new website... using vuejs

--- app.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-http.createServer((req, res) => {
-  res.write("On the way to being a full stack engineer!");
-  res.end();	
-}
-).listen(3000);
-
-console.log("Server started on port http://127.0.0.1:3000");
---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="title">alain</div>
+</template>
+
+<style scoped>
+  .title {
+    color: palevioletred;
+  }
+</style>
