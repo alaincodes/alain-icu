881f75c918e516cf615140ac35c17181e935b923
Alain Tran <alaint77@gmail.com>
2023-05-22 21:51:52 +0200

    testing new colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'forest-green': '#0B421A',
+        'old-lace': '#fcf6e3',
+        'philippine-orange': '#e67219',
+        'prussian-blue': '#00334E',
+      },
+    },
     screens: {
       'xs': '480px',
     },
