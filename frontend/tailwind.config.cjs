8e3d7a5dde16ef48fddd13e51cc6bec35d3daccf
Alain Tran <alaint77@gmail.com>
2023-05-27 14:56:38 +0200

    add neon text animation... TODO: adjust color for a11y

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -8,6 +8,19 @@ module.exports = {
         'blue': '#001233',
         'coral-red': '#ff595a'
       },
+			keyframes: {
+        pulsate: {
+					'100%': { 
+						'text-shadow': '0 0 4px #ffa8a8, 0 0 11px #ffa8a8, 0 0 19px #ffa8a8, 0 0 40px #ffa8a8, 0 0 80px #ff595a, 0 0 90px #ff595a, 0 0 100px #ff595a, 0 0 150px #ff595a'
+					},
+  				'0%': {
+    				'text-shadow': '0 0 4px #ffa8a8, 0 0 10px #ffa8a8, 0 0 18px #ffa8a8, 0 0 38px #ffa8a8, 0 0 73px #ff595a, 0 0 80px #ff595a, 0 0 94px #ff595a, 0 0 140px #ff595a'
+					},
+        }
+      },
+			animation: {
+        pulsate: 'pulsate 1s ease-in-out infinite',
+      }
 		},
 	},
 	plugins: [],
