0b17b5e45ab0dfe477af873a767abb55f73541eb
Alain Tran <alaint77@gmail.com>
2024-06-16 15:33:27 +0200

    add site colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				beige: "#efe0ca",
+				blue: "#001233",
+				"coral-red": "#ff595a",
+			},
+		},
 	},
 	plugins: [],
 };
