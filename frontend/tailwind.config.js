33ac14f63080b540d897d6ed52240c1a09dbc8e3
Alain Tran <alaint77@gmail.com>
2024-06-21 20:09:08 +0200

    add heartbeat animation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,34 @@ export default {
 				blue: "#001233",
 				"coral-red": "#ff595a",
 			},
+			keyframes: {
+				heartbeat: {
+					from: {
+						transform: "scale(1)",
+						"transform-origin": "center center",
+						"animation-timing-function": "ease-out",
+					},
+					"10%": {
+						transform: "scale(0.91)",
+						"animation-timing-function": "ease-in",
+					},
+					"17%": {
+						transform: "scale(0.98);",
+						"animation-timing-function": "ease-out;",
+					},
+					"33%": {
+						transform: "scale(0.87);",
+						"animation-timing-function": "ease-in;",
+					},
+					"45%": {
+						transform: "scale(1);",
+						"animation-timing-function": "ease-out;",
+					},
+				},
+			},
+			animation: {
+				heartbeat: "heartbeat 2s ease-in-out infinite",
+			},
 		},
 	},
 	plugins: [],
