6855a5f041fe0aeb2f1f51ed3bae67ff1e450827
Alain Tran <alaint77@gmail.com>
2024-07-13 00:14:39 +0200

    playing with vuejs

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,45 +1,56 @@
 <script setup>
+import { reactive } from 'vue'
 import { RouterLink } from 'vue-router'
+
+const projects = reactive({
+  ciamt: {
+    name: 'Ciamt',
+    url: 'https://www.ciamt.org'
+  },
+  beneteau: {
+    name: 'Beneteau',
+    url: 'https://www.beneteau.com'
+  },
+  delphia: {
+    name: 'Delphia',
+    url: 'https://www.delphiayachts.com'
+  },
+  'Lustucru Selection': {
+    name: 'Lustucru Selection',
+    url: 'https://www.lustucru-selection.fr'
+  },
+  Opsone: {
+    name: 'Opsone',
+    url: 'https://www.opsone.net'
+  },
+  'Panzani Food Service': {
+    name: 'Panzani Food Service',
+    url: 'https://www.panzanifoodservice.com'
+  },
+  'Belair Monange': {
+    name: 'Belair Monange',
+    url: 'https://www.belairmonange.com'
+  }
+})
 </script>
+
 <template>
-  <main class="space-y-2 grid place-content-center h-dvh p-6 bg-blue text-coral-red text-center">
+  <main
+    class="space-y-2 grid place-content-center h-dvh p-6 bg-blue text-xl text-coral-red text-center"
+  >
     <nav>
       <ul>
-        <li>
-          <a href="https://www.ciamt.org" target="_blank" class="hover:text-beige">Ciamt</a>
-        </li>
-        <li>
-          <a href="https://www.beneteau.com" target="_blank" class="hover:text-beige">Beneteau</a>
-        </li>
-        <li>
-          <a href="https://www.delphiayachts.com" target="_blank" class="hover:text-beige"
-            >Delphia Yachts</a
-          >
-        </li>
-        <li>
-          <a href="https://www.lustucru-selection.fr" target="_blank" class="hover:text-beige"
-            >Lustucru Selection</a
-          >
-        </li>
-        <li>
-          <a href="https://www.opsone.net" target="_blank" class="hover:text-beige">Opsone</a>
-        </li>
-        <li>
-          <a href="https://www.panzanifoodservice.com" target="_blank" class="hover:text-beige"
-            >Panzani Food Service</a
-          >
-        </li>
-        <li>
-          <a href="https://www.belairmonange.com" target="_blank" class="hover:text-beige"
-            >Belair Monange</a
-          >
+        <li v-for="(project, index) in projects" :key="index">
+          <a :href="project.url" target="_blank" class="hover:text-beige">{{ project.name }}</a>
         </li>
       </ul>
     </nav>
 
     <div>
-      <button class="text-xl font-medium hover:text-beige duration-300 animate-heartbeat">
-        <RouterLink to="/">Retour</RouterLink>
+      <button
+        class="text-xl font-medium hover:text-coral-red duration-300 animate-heartbeat text-beige"
+      >
+        <RouterLink to="/">Go back</RouterLink>
       </button>
     </div>
   </main>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,14 +3,17 @@ import { RouterLink } from 'vue-router'
 </script>
 <template>
   <main class="space-y-2 grid place-content-center h-dvh p-6 bg-blue text-coral-red text-center">
-    <a href="https://www.linkedin.com" target="_blank" class="hover:text-beige duration-300">
-      <h1 class="text-2xl font-bold">Alain TRAN</h1>
-    </a>
-    <div>
-      <button class="text-xl font-medium hover:text-beige duration-300 animate-heartbeat">
-        <RouterLink to="/about">Frontend developer</RouterLink>
-      </button>
+    <div
+      class="grid grid-cols-1 place-items-center gap-4 p-6 rounded-lg border border-solid border-coral-red"
+    >
+      <div>
+        <a href="https://www.linkedin.com" target="_blank" class="hover:text-beige duration-300">
+          <h1 class="text-2xl font-bold">Alain TRAN</h1>
+        </a>
+        <button class="text-xl font-medium hover:text-beige duration-300 animate-heartbeat">
+          <RouterLink to="/about">Frontend developer</RouterLink>
+        </button>
+      </div>
     </div>
-    <p class="text-xs font-bold">LOL</p>
   </main>
 </template>
